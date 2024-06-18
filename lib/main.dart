357f6a1fd1b6d38cfde5c7ed555549153b1314d0
Dylan Kavundama <dylancorporation.cd@gmail.com>
2024-06-18 13:25:07 +0200

    add

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:admin_ib/style.dart';
+
+import 'Profil/produit.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // MobileAds.instance.initialize();
+
+  await Firebase.initializeApp();
+  await Future.delayed(const Duration(seconds: 1));
+  // FlutterNativeSplash.remove();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.red,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: CouleurPrincipale,
+          useMaterial3: false,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: CouleurPrincipale),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const Produit());
+  }
+}
